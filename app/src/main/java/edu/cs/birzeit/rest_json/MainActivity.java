58bdb3991c22a387f70f61957d66c39707f0dec9
szainbzu <szain@birzeit.edu>
2021-10-18 12:52:48 +0200

    version1

---
+++ app/src/main/java/edu/cs/birzeit/rest_json/MainActivity.java
@@ -0,0 +1,74 @@
+package edu.cs.birzeit.rest_json;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RequestQueue queue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        queue = Volley.newRequestQueue(this);
+    }
+
+    public void btnSearch_Click(View view) {
+        EditText edtCat = findViewById(R.id.edtCat);
+        ListView lst = findViewById(R.id.lstBooks);
+        String cat = edtCat.getText().toString();
+        String url = "http://10.0.2.2:84/rest/info_json.php?cat=" + cat;
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url,
+                null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                ArrayList<String> books = new ArrayList<>();
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject obj = response.getJSONObject(i);
+                        books.add( obj.getString("title"));
+                    }catch(JSONException exception){
+                        Log.d("Error", exception.toString());
+                    }
+                }
+                String[] arr = new String[books.size()];
+                arr = books.toArray(arr);
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                        MainActivity.this, android.R.layout.simple_list_item_1,
+                        arr);
+                lst.setAdapter(adapter);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Toast.makeText(MainActivity.this, error.toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        queue.add(request);
+
+    }
+}
